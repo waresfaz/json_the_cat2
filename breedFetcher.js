851ebebf8c0dec29e28a198157fcaaaca777a5cc
Wares <fazelyar.wares@gmail.com>
2020-04-12 22:33:38 +0000

    handles errors and invalid breed entries

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,10 +2,24 @@ request = require("request");
 const args = process.argv.slice(2);
 const breed = args[0]
 
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, reponse, body) {
-    // console.log(typeof body)
-    // console.log("info:", body)
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+  
+  const data = JSON.parse(body);
+  // console.log(data[0]);
+  // console.log(error);
+  
+  if (data.length === 0) {
+    console.log('Breed not found');
+  }
+  if (error) {
+  console.error("there has been an error:", error);
+  }
+  if(body) {
     const data = JSON.parse(body);
-    console.log(data[0]);
-    // console.log(typeof data);
-  });
+    if(data && data[0]) {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+      console.log(typeof data);
+    }
+  }
+});
