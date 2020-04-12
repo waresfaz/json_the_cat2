3ffc13133cf22ef36d6ecb0e7c6f8e5611518b38
Wares <fazelyar.wares@gmail.com>
2020-04-12 22:50:40 +0000

    put code into function and exported to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,29 @@
 request = require("request");
-const args = process.argv.slice(2);
-const breed = args[0]
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-  
-  const data = JSON.parse(body);
-  // console.log(data[0]);
-  // console.log(error);
-  
-  if (data.length === 0) {
-    console.log('Breed not found');
-  }
-  if (error) {
-  console.error("there has been an error:", error);
-  }
-  if(body) {
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    
     const data = JSON.parse(body);
-    if(data && data[0]) {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
-      console.log(typeof data);
+    // console.log(data[0]);
+    // console.log(error);
+    
+    if (data.length === 0) {
+      console.log('Breed not found');
     }
-  }
-});
+    if (error) {
+    console.error("there has been an error:", error);
+    }
+    if(body) {
+      const data = JSON.parse(body);
+      if(data && data[0]) {
+        const data = JSON.parse(body);
+        console.log(data[0].description);
+        console.log(typeof data);
+      }
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
